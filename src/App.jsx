4d84c56e4021b679699e745fdf2a68fb517475c2
Ujjwal Darnal <59062@edu.nielsbrock.dk>
2026-05-06 01:34:33 +0200

    clean vite starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App(){
   return(
-<>
-</>
+<div>
+  <header>
+    <h1>Job Tracker</h1>
+    <p>Track you job applications and stay organized.</p>
+  </header>
+</div>
   )
 }
 export default App
